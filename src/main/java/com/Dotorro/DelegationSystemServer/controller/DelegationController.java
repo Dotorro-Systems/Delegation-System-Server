c3e08a5a9195e36e983145d7c9106813b7b4b219
WandaPtasiewicz <wanda.ptasiewicz@gmail.com>
2025-03-09 20:43:32 +0100

    Add: delegation entity

---
+++ src/main/java/com/Dotorro/DelegationSystemServer/controller/DelegationController.java
@@ -0,0 +1,23 @@
+package com.Dotorro.DelegationSystemServer.controller;
+
+import com.Dotorro.DelegationSystemServer.model.Delegation;
+import com.Dotorro.DelegationSystemServer.service.DelegationService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/delegations")
+@CrossOrigin(origins = "*") // Allow frontend requests
+public class DelegationController {
+
+    private final DelegationService delegationService;
+
+    public DelegationController(DelegationService delegationService){this.delegationService = delegationService;}
+
+    @GetMapping
+    public List<Delegation> getDelegations(){return delegationService.getAllDelegations();}
+
+    @PostMapping
+    public Delegation createDelegation(@RequestBody Delegation delegation){
+        return delegationService.createDelegation(delegation);}
+}

be3f5c102f04b8b6d605dff7832dba91bc716bc4
Maciej Góralczyk <goralczm@gmail.com>
2025-03-16 19:56:55 +0100

    Add: expense entity

---
+++ src/main/java/com/Dotorro/DelegationSystemServer/service/ExpenseService.java
@@ -0,0 +1,24 @@
+package com.Dotorro.DelegationSystemServer.service;
+
+import com.Dotorro.DelegationSystemServer.model.Expense;
+import com.Dotorro.DelegationSystemServer.repository.ExpenseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ExpenseService {
+    private final ExpenseRepository expenseRepository;
+
+    public ExpenseService(ExpenseRepository expenseRepository) {
+        this.expenseRepository = expenseRepository;
+    }
+
+    public List<Expense> getAllExpenses() {
+        return expenseRepository.findAll();
+    }
+
+    public Expense createExpense(Expense expense) {
+        return expenseRepository.save(expense);
+    }
+}

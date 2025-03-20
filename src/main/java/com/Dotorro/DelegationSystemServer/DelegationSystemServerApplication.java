9d96a99d7c659cc0d27b1ed96d0c595af30f1ccb
Maciej Góralczyk <goralczm@gmail.com>
2025-03-20 16:53:06 +0100

    Add: .env variables loading and fix .env sprinboot connection

--- src/main/java/com/Dotorro/DelegationSystemServer/DelegationSystemServerApplication.java
+++ src/main/java/com/Dotorro/DelegationSystemServer/DelegationSystemServerApplication.java
@@ -1,5 +1,6 @@
 package com.Dotorro.DelegationSystemServer;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 public class DelegationSystemServerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(DelegationSystemServerApplication.class, args);
 	}
 

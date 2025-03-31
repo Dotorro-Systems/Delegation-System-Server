dd6c80e44e95c931f38f4775f28249b55fea7818
WandaPtasiewicz <wanda.ptasiewicz@gmail.com>
2025-03-31 09:23:33 +0200

    Improve: generateToken function

--- src/main/java/com/Dotorro/DelegationSystemServer/service/AuthorizationService.java
+++ src/main/java/com/Dotorro/DelegationSystemServer/service/AuthorizationService.java
@@ -5,9 +5,11 @@ import com.Dotorro.DelegationSystemServer.model.User;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 
@@ -16,26 +18,30 @@ public class AuthorizationService {
     public static String SECRET_KEY = "905310c638dfbc8c48f27a4d34ab725e1cf3cc82136ffc09c4aaa27dd181d972";
     public static long EXPIRATION_TIME_IN_MS = 1000 * 60 * 60; // 1 h
 
-    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    //private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 
     public String generateToken(UserDTO userDTO){
         return Jwts.builder()
-                .setSubject(userDTO.getEmail())
+                .subject(userDTO.getEmail())
                 .claim("role", userDTO.getRole())
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME_IN_MS))
-                .signWith(key, SignatureAlgorithm.HS256)
+                .signWith(getSignInKey(), Jwts.SIG.HS256)
                 .compact();
     }
 
     public String extractEmail(String token) {
-        return Jwts.parser().setSigningKey(key).build()
+        return Jwts.parser().setSigningKey(getSignInKey()).build()
                 .parseClaimsJws(token).getBody().getSubject();
     }
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(key).build().parseClaimsJws(token);
+            Jwts.parser().setSigningKey(getSignInKey()).build().parseClaimsJws(token);
             return true;
         } catch (JwtException e) {
             return false;

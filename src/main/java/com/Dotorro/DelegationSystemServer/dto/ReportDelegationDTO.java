6e6d516ae8690224a0967e58b0e83f9a642865cb
michal-kutryb <m.kutryb@outlook.com>
2025-05-01 21:17:14 +0200

    Add: ReportDTO file

---
+++ src/main/java/com/Dotorro/DelegationSystemServer/dto/ReportDelegationDTO.java
@@ -0,0 +1,118 @@
+package com.Dotorro.DelegationSystemServer.dto;
+
+import com.Dotorro.DelegationSystemServer.model.Delegation;
+import com.Dotorro.DelegationSystemServer.model.Note;
+import com.Dotorro.DelegationSystemServer.model.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+public class ReportDelegationDTO {
+    private String title;
+    private Integer allWorkHours;
+    private Map<User,Integer> userAllWorkHours;
+    private Double totalExpenses;
+    private String destination;
+    private List<Note> allNotes;
+    private String departmentName;
+    private List<User> allUsers;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    public ReportDelegationDTO(Delegation delegation, Map<User,Integer> userAllWorkHours, Integer allWorkHours, Double totalExpenses, List<Note> allNotes, List<User> allUsers)
+    {
+        this.title = delegation.getTitle();
+        this.destination = delegation.getDestination();
+        this.startDate = delegation.getStartDate();
+        this.endDate = delegation.getEndDate();
+        this.userAllWorkHours = userAllWorkHours;
+        this.allWorkHours = allWorkHours;
+        this.totalExpenses = totalExpenses;
+        this.allNotes = allNotes;
+        this.allUsers = allUsers;
+        this.departmentName = delegation.getDepartment().getName();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getAllWorkHours() {
+        return allWorkHours;
+    }
+
+    public void setAllWorkHours(Integer allWorkHours) {
+        this.allWorkHours = allWorkHours;
+    }
+
+    public Double getTotalExpenses() {
+        return totalExpenses;
+    }
+
+    public void setTotalExpenses(Double totalExpenses) {
+        this.totalExpenses = totalExpenses;
+    }
+
+    public Map<User, Integer> getUserAllWorkHours() {
+        return userAllWorkHours;
+    }
+
+    public void setUserAllWorkHours(Map<User, Integer> userAllWorkHours) {
+        this.userAllWorkHours = userAllWorkHours;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public List<Note> getAllNotes() {
+        return allNotes;
+    }
+
+    public void setAllNotes(List<Note> allNotes) {
+        this.allNotes = allNotes;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public List<User> getAllUsers() {
+        return allUsers;
+    }
+
+    public void setAllUsers(List<User> allUsers) {
+        this.allUsers = allUsers;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+}
+
+
